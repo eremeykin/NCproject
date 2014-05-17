e9dc5672a4dbbb5ddd5ff722657009d7f1fa6427
eremeykin <eremeykin@gmail.com>
2014-05-17 21:44:49 +0400

    Добавлены сессии. LoginPage.jsp - страница с формой регистрации/входа Logout.jsp - код выхода MainUserPage.jsp - основная страница конкретного пользователя REGsuccessed.jsp -страница успешной регистрации

--- src/java/pete/users/usersServlet.java
+++ src/java/pete/users/usersServlet.java
@@ -5,9 +5,7 @@ import java.io.PrintWriter;
 import java.sql.*;
 import java.util.Enumeration;
 import java.util.Properties;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 
 public class usersServlet extends HttpServlet {
 
@@ -21,7 +19,8 @@ public class usersServlet extends HttpServlet {
         this.process(request, response);
     }
 
-    private void Submitprocessor(HttpServletRequest request, HttpServletResponse response, PrintWriter out) {
+    private void loginProcessor(HttpServletRequest request, HttpServletResponse response, PrintWriter out) throws IOException {
+        //connect to the database
         String msUrl = "jdbc:mysql://localhost:3306/users";
         Properties msProps = new Properties();
         msProps.setProperty("user", "root");
@@ -32,8 +31,8 @@ public class usersServlet extends HttpServlet {
             Statement msStatement = msConnection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             Enumeration en = request.getParameterNames();
             String login = null, password = null;
+            //record  request parameters into variables
             while (en.hasMoreElements()) {
-                // Get the name of the request parameter
                 String name = (String) en.nextElement();
                 if (name.equals("Login")) {
                     login = request.getParameter(name);
@@ -45,16 +44,20 @@ public class usersServlet extends HttpServlet {
             ResultSet rs = msStatement.executeQuery("SELECT password FROM login_password WHERE login = \"" + login + "\"");
             if (rs.next() && password.equals(rs.getString(1))) {
                 out.println("Success! :))");
+                //get session
+                HttpSession hs = request.getSession(true);
+                hs.setAttribute("Login", login);
+                response.sendRedirect("MainUserPage.jsp");
             } else {
                 out.println("Wrong login/password! :(");
             }
-            //response.sendRedirect("REGsuccessed.jsp");
         } catch (SQLException ex) {
             out.println(ex);
         }
     }
 
-    private void Registerprocessor(HttpServletRequest request, HttpServletResponse response, PrintWriter out) {
+    private void registerProcessor(HttpServletRequest request, HttpServletResponse response, PrintWriter out) {
+        //connect to the database
         String msUrl = "jdbc:mysql://localhost:3306/users";
         Properties msProps = new Properties();
         msProps.setProperty("user", "root");
@@ -65,8 +68,8 @@ public class usersServlet extends HttpServlet {
             Statement msStatement = msConnection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             Enumeration en = request.getParameterNames();
             String login = null, password = null;
+            //record  request parameters into variables
             while (en.hasMoreElements()) {
-                // Get the name of the request parameter
                 String name = (String) en.nextElement();
                 if (name.equals("Login")) {
                     login = request.getParameter(name);
@@ -83,61 +86,22 @@ public class usersServlet extends HttpServlet {
     }
 
     /*
-     generate the page showing all the request parameters
+     Select processor
      */
     private void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setStatus(200);
         PrintWriter out = response.getWriter();
         response.setContentType("text/html");
-
         Enumeration en = request.getParameterNames();
         while (en.hasMoreElements()) {
             String name = (String) en.nextElement();
-            String value = request.getParameter(name);
             if (name.equals("Submit")) {
-                Submitprocessor(request, response, out);
+                loginProcessor(request, response, out);
             }
             if (name.equals("Register")) {
-                Registerprocessor(request, response, out);
+                registerProcessor(request, response, out);
             }
         }
 
-        //
-//        String msUrl = "jdbc:mysql://localhost:3306/users";
-//        Properties msProps = new Properties();
-//        msProps.setProperty("user", "root");
-//        msProps.setProperty("password", "1580");
-//        try {
-//            Connection msConnection = DriverManager.getConnection(msUrl, msProps);
-//            Statement msStatement = msConnection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-//            ResultSet rs = msStatement.executeQuery("SELECT * FROM login_password");
-//            while (rs.next()) {
-//                //int foo = rs.getInt(1);
-//                out.println(rs.getString(1));
-//                out.println(rs.getString(2));
-//            }
-//        } catch (SQLException ex) {
-//            out.println(ex);
-//        }
-        //out.println(groupsResultSet.getString(1));
-        // Get the values of all request parameters
-        //////////////////////////////////////////////
-//        Enumeration en = request.getParameterNames();
-//        while (en.hasMoreElements()) {
-//            // Get the name of the request parameter
-//            String name = (String) en.nextElement();
-//            out.println(name);
-//
-//            // Get the value of the request parameter
-//            String value = request.getParameter(name);
-//
-//            // If the request parameter can appear more than once in the query string, get all values
-//            String[] values = request.getParameterValues(name);
-//
-//            for (int i = 0; i < values.length; i++) {
-//                out.println(" " + values[i]);
-//            }
-//        }
-//        out.close();
     }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -1,55 +1,23 @@
-<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<%@taglib prefix="sql" uri="http://java.sun.com/jsp/jstl/sql"%>
 <%-- 
     Document   : index
-    Created on : 10.05.2014, 14:21:03
+    Created on : 17.05.2014, 19:44:44
     Author     : Pete
 --%>
-
-
-
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Login page</title>
+        <title>JSP Page</title>
     </head>
-    <body>
-        <h1><center>Login page</h1>
-        <div align = "center">
-            <form action="usersServlet">
-                <table border="1">
-                    <thead>
-                        <tr>
-                            <th colspan="2">Please enter your login and password.</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Login:</td>
-                            <td>
-                                <input type="text" name="Login" value="" size="60" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Password:</td>
-                            <td>
-                                    <input type="text" name="Password" value="" size="60" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2"> 
-                    <center>
-                        <input type="submit" value="Login" name="Submit" />
-                        <input type="submit" value="Register" name="Register" />
-                    </center>                           
-                    </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </form>
-        </div>
-    </body>
+    <%
+        HttpSession hs = request.getSession(true);
+        String name = (String) hs.getAttribute("Login");
+        if (name != null) {
+            response.sendRedirect("MainUserPage.jsp");
+        } else {
+            response.sendRedirect("LoginPage.jsp");
+        }
+    %>
 </html>
